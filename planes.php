1c9fd7e58e328177874e8fdefe527e701f2cf877
marshall87xxx <97808352+marshall87xxx@users.noreply.github.com>
2023-12-11 13:25:36 -0600

    Delete planes.php

--- planes.php
+++
@@ -1,6 +0,0 @@
-<?php require('./layout/header.php')?>
-<?php require('./layout/modal.php')?>
-<?php require('./layout/simulador.php')?>
-<?php require('./layout/footer.php')?>
-<script src="./js/select.js"></script>
-<script src="./js/operaciones.js"></script>
